80411b383f26189b721c57bb76831ecd4bdd0633
Hoang Nguyen <hoang@wetrust.io>
2020-07-03 17:14:21 -0700

    support single command

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # manage-node-scripts
+
 A collection of scripts to manage Ethereum nodes
 
 ### Download geth
+
+- A specific version
+
 ```
-./dowload-geth 1.8.23
-# The downloaded binary is in the same directory
-sudo cp geth /usr/local/bin/
+wget --no-cache -qO- https://raw.githubusercontent.com/WeTrustPlatform/manage-node-scripts/master/download-geth.sh | bash -s -- 1.9.15
+```
+
+- Latest geth
+
+```
+wget --no-cache -qO- https://raw.githubusercontent.com/WeTrustPlatform/manage-node-scripts/master/download-geth.sh | bash
+```
+
+- Copy the binary to path
+
+```
+  sudo cp geth /usr/local/bin/
 ```
 
 Why do I need this script?
+
 - It verifies MD5 checksums.
 - It verifies GPG signatures.
 - It unzips for you.
--- download-geth.sh
+++ download-geth.sh
@@ -20,8 +20,65 @@ DOWNLOAD_PATH=https://gethstore.blob.core.windows.net/builds
 # no-verbose no-clobber
 WGET="wget -nc -nv"
 
-# this script set values of version, file_name and md5_value
-source ./search.sh
+# find version, file_name and md5_value
+version=$1
+if [ -z "$1" ] ; then
+  version=$(curl -s https://api.github.com/repos/ethereum/go-ethereum/releases/latest | grep tag_name |  sed 's/.*"v\(.*\)".*/\1/')
+  echo ">> No version specified. Use the latest $version"
+fi
+
+case `uname` in
+  'Linux')
+    prefix=geth-linux-amd64-$version
+    ;;
+  'Darwin')
+    prefix=geth-darwin-amd64-$version
+    ;;
+  *)
+    echo ">> Unknown OS!"
+    exit 1
+    ;;
+esac
+
+SEARCH_QUERY=https://gethstore.blob.core.windows.net/builds\?restype\=container\&comp\=list\&maxresults\=1\&prefix\=$prefix
+
+echo $SEARCH_QUERY
+
+read_dom () {
+  local IFS=\>
+  read -d \< ENTITY CONTENT
+}
+
+parse_dom () {
+  if [[ $ENTITY = "Content-MD5" ]] ; then
+    md5_value=$(echo -n $CONTENT | base64 --decode | xxd -p)
+  fi
+
+  if [[ $ENTITY = "Name" ]] ; then
+    file_name=$CONTENT
+  fi
+}
+
+parse_query_results () {
+  md5_content=""
+  file_name=""
+  while read_dom ; do
+    parse_dom
+  done
+  echo $file_name $md5_value
+}
+
+get_file_name_and_md5 () {
+  curl $SEARCH_QUERY | parse_query_results
+}
+
+# assume file_name and md5_content content no spaces
+read file_name md5_value < <(get_file_name_and_md5)
+
+if [ -z "$file_name" ] ; then
+  echo ">> Cannot find $prefix"
+  exit 1
+fi
 
 case `uname` in
   'Linux')
@@ -79,6 +136,7 @@ echo ">> Unpacking tar"
 tar xvf $file_name
 mv $(basename $file_name .tar.gz)/geth ../
 rm -r $(basename $file_name .tar.gz)
-echo ">> Done! Please feel free to remove ./tmp"
+echo ">> Done!"
+echo ">> Please feel free to rm -r ./tmp"
 echo ">> Please manually sudo cp geth /usr/local/bin/"
 exit 0
